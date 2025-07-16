79431f38d071cc4152cca6bac17863ca7261b738
Brian Tifa <brian.tifa08@gmail.com>
2025-07-15 20:04:41 -0400

    estructura del api y archivo de datos

---
+++ app/data/profile_data.py
@@ -0,0 +1,33 @@
+profile_data = {
+    "name": "Brian Jesús Tifá Rodríguez",
+    "title": "Python Developer",
+    "location": "Santo Domingo, República Dominicana",
+    "email": "brian.tifa08@gmail.com",
+    "linkedin": "https://www.linkedin.com/in/brian-tifa-02b7b5196",
+    "github": "https://github.com/BJtifa",
+    "skills": ["Python", "FastAPI", "SQL", "NoSQL", "Git", ".NET", "C#"],
+    "experience": [
+        {
+            "position": "Programador Senior Visual",
+            "company": "Banco BHD",
+            "years": "2023 - Actualidad"
+        },
+        {
+            "position": "Programador Visual",
+            "company": "Banco BHD",
+            "years": "2022 - 2023"
+        },
+        {
+            "position": "Desarrollador de software",
+            "company": "Glowicom",
+            "years": "2021"
+        }
+    ],
+    "projects": [
+        {
+            "name": "Organizador de Archivos",
+            "description": "Script en Python para renombrar archivos automáticamente.",
+            "url": "https://github.com/BJtifa/file-renamer"
+        }
+    ]
+}
